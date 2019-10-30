71901ed81971fb8fa9ae0fec7e37f1e5d5dc849b
smiromania <45713280+smiromania@users.noreply.github.com>
2019-10-30 09:14:57 +0200

    44

--- 1.cpp
+++ 1.cpp
@@ -6,15 +6,15 @@ int main()
 {
 
   char s[256];
-  int i, j, cacat;
+  int i, j, c;
   cin.get(s, 256);
   for (i = 0; i < strlen(s); i++)
     if (s[i] >= 'a' && s[i] <= 'z') {
-      cacat = 0;
+      c = 0;
       for (j = 0; j < i; j++)
         if (s[i] == s[j])
-          cacat = 1;
-      if (!cacat)
+          c = 1;
+      if (!c)
         cout << s[i] << " ";
     }
 }
