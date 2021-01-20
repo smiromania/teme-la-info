88ab9435f002386ad8e7aeaf878711beef07ce71
smiromania <45713280+smiromania@users.noreply.github.com>
2021-01-20 12:27:41 +0200

    Update V62_S3_3.cpp

--- V62_S3_3.cpp
+++ V62_S3_3.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 
 using namespace std;
-void fibo(int n, long v[30])
+void fibo(int n, long long v[30])
 {
-    v[0] = 1;
-    v[1] = 1;
-    for (int i = 2; i < n; i++)
-        v[i] = v[i - 1] + v[i - 2];
+    long long a[100], j = 0;
+    a[0] = 1;
+    a[1] = 1;
+    for (int i = 2; i < n * 3 + 2; i++)
+    {
+        a[i] = a[i - 1] + a[i - 2];
+        if (a[i] % 2)
+        {
+            v[j] = a[i]; j++;
+            cout << v[j - 1] << endl;
+        }
+        if (j == n)
+            i = n * 4;
+    }
 }
 
 int main()
 {
-    long v[30];
+    long long v[30];
     fibo(30, v);
     for (int i = 0; i < 30; i++)
         cout << v[i]<<" ";
+    return 0;
 }
